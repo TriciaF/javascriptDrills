acf90a7f3bc66e0ac1387996003ee4733381baa9
TriciaF <TriciaF@thinkful.com>
2017-12-04 17:18:04 -0500

    added rockpaperscissors function

--- fileTest.js
+++ fileTest.js
@@ -37,25 +37,21 @@ function beyond(num) {
 
 function theCode(list) {
     let dividedList = list.split(" ");
-    for (let i=0; i<dividedList.length; i++) {
+    for (let i = 0; i < dividedList.length; i++) {
         if (dividedList[i].charAt(0) === 'a') {
             console.log(dividedList[i].charAt(1));
-        }
-        else if (dividedList[i].charAt(0) === 'b') {
+        } else if (dividedList[i].charAt(0) === 'b') {
             console.log(dividedList[i].charAt(2));
-        }
-        else if (dividedList[i].charAt(0) === 'c') {
+        } else if (dividedList[i].charAt(0) === 'c') {
             console.log(dividedList[i].charAt(3));
-        }
-        else if (dividedList[i].charAt(0) === 'd') {
+        } else if (dividedList[i].charAt(0) === 'd') {
             console.log(dividedList[i].charAt(4));
-        }
-         else {console.log(" ")}
+        } else { console.log(" ") }
     }
 }
 
 function daysOfMonth(name) {
-    switch(name){
+    switch (name) {
         case 'January':
         case 'March':
         case 'May':
@@ -65,20 +61,59 @@ function daysOfMonth(name) {
         case 'December':
             console.log(`${name} has 31 days`);
             break;
-        
+
         case 'February':
             console.log(`${name} has 28 days`);
             break;
-        
+
         default:
             console.log(`${name} has 30 days`);
     }
 }
 
+function RockPaperScissors(userChoice) {
+    var computer = 1 + Math.floor((Math.random() * 3));
+    return computer;
 
+}
+
+function playRockPaperScissors(userChoice) {
+    let computer = RockPaperScissors(userChoice);
+
+    switch (userChoice) {
+        case 1:
+            if (computer === 2) {
+                console.log('Paper beats Rock - Computer Wins');
+                break;
+            } else if (computer === 3) {
+                console.log('Rock beats Scissors - User Wins');
+                break;
+            }
+        case 2:
+            if (computer === 1) {
+                console.log('Paper beats Rock - User Wins');
+                break;
+            } else if (computer === 3) {
+                console.log('Scissors beats paper - Computer Wins');
+                break;
+            }
+        case 3:
+            if (computer === 1) {
+                console.log('Rock beats Scissors - Computer Wins');
+                break;
+            } else if (computer === 2) {
+                console.log('Paper beats Rock - User Wins');
+                break;
+            }
+        default:
+            console.log("It's a tie - play again");
+    }
+
+}
 
 whoAmI("Sam", 28);
 jediName("Tricia", "Forrester");
 beyond(Math.log(0));
 theCode('craft block argon meter bells brown croon droop');
 daysOfMonth('December');
+playRockPaperScissors(3);
