083e9eb92c49268d65d70b664162e391e9816fcf
Sam Gould <sjg326@gmail.com>
2017-12-04 12:48:58 -0800

    added try/catch statements and verifying data types

--- fileTest.js
+++ fileTest.js
@@ -1,12 +1,24 @@
 'use strict';
 
-function whoAmI(name, age) {
-    let _name = "Tricia" || name;
-    let _age = "54" || age;
-    let yearOfBirth = 2017 - age;
+function yearOfBirth(age) {
+    if (age < 0) throw new Error('Age cannot be negative');
 
-    console.log(`Hi my name is ${name} and I am ${age} years old`);
-    console.log(`I was born in ${yearOfBirth}`);
+    else {return 2017 - age;}
+}
+
+function whoAmI(name, age) {
+    if (typeof name === 'string' && typeof age === 'number') {
+
+        try {
+            let yob=yearOfBirth(age);
+            console.log(`Hi my name is ${name} and I am ${age} years old`);
+            console.log(`I was born in ${yob}`);
+        }
+        catch(Error){
+            console.log(Error);
+        }
+    }
+    else {console.log('Arguments not valid');}
 }
 
 whoAmI("Sam", 28);
