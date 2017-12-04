e89cd0498d42fece58ef9f23952e6f330ab1e8c3
TriciaF <TriciaF@thinkful.com>
2017-12-04 15:03:36 -0500

    added function signature and yearOfBirth variable

--- fileTest.js
+++ fileTest.js
@@ -1,7 +1,12 @@
 'use strict';
 
-function whoAmI(){
-console.log('Hi my name is Tricia and I am 54 years old');
+function whoAmI(name, age) {
+    let _name = "Tricia" || name;
+    let _age = "54" || age;
+    let yearOfBirth = 2017 - age;
+
+    console.log(`Hi my name is ${name} and I am ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
 }
 
-whoAmI();
+whoAmI("Sam", 28);
